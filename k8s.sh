218a48030b250ab8fa60fe5c0da8469a6963deef
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-02-24 09:50:47 -0800

    Create k8s.sh

---
+++ k8s.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Desabilitando swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Instalando o Docker
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Adicionando repositório do Kubernetes
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Instalando o Kubernetes
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+# Configurando o Kubernetes
+sudo kubeadm init --
